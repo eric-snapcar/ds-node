f58ff4023e058cf14e672c97545a55f1e4bc87b3
Eric Hth <eric@snapcar.com>
2018-01-08 00:51:53 +0100

    app.js ... python.py ...

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.get('/', function (req, res) {
 app.use(express.static('build'));
 app.get('/test', function (req, res) {
    res.json({ message: 'TEST API OK' });
+
+
+
+// TEST
+   var spawn = require('child_process').spawn,
+       py    = spawn('python', ['python.py']),
+       data = [1,2,3,4,5,6,7,8,9],
+       dataString = '';
+
+   py.stdout.on('data', function(data){
+     console.log("TEST stdout.on data");
+     console.log(data);
+     dataString += data.toString();
+   });
+   py.stdout.on('end', function(){
+     console.log('Sum of numbers = ',dataString);
+   });
+   py.stdin.write("TEST WRITE");
+   py.stdin.end();
+//  
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
